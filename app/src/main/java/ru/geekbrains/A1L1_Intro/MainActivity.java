378d382646e29d1a65081e67dfa3164b43e0c7a0
Sergey <Ganzalo2010@gmail.com>
2019-11-25 17:40:20 +0300

    dz 1 done

---
+++ app/src/main/java/ru/geekbrains/A1L1_Intro/MainActivity.java
@@ -0,0 +1,78 @@
+package ru.geekbrains.A1L1_Intro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Locale;
+
+import static android.graphics.ColorSpace.Model.XYZ;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button engButton;
+    private Button ruButton;
+    private Button startButton;
+
+    private TextView welcomeTextView;
+    private TextView helloWorldTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        init();
+        changTextButtonClick();
+    }
+
+    private void init() {
+        engButton = findViewById(R.id.engButton);
+        ruButton = findViewById(R.id.rusButton);
+        startButton = findViewById(R.id.startButton);
+
+        welcomeTextView = findViewById(R.id.welcomeTextView);
+        helloWorldTextView = findViewById(R.id.hellowWorldTextView);
+    }
+
+    private void changTextButtonClick() {
+        engButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Locale locale = new Locale("en");
+                Locale.setDefault(locale);
+                Configuration config = new Configuration();
+                config.locale = locale;
+                getBaseContext().getResources().updateConfiguration(config, getBaseContext().getResources().getDisplayMetrics());
+
+                Intent intent = new Intent(MainActivity.this, MainActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+
+            }
+        });
+
+        ruButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Locale locale = new Locale("ru");
+                Locale.setDefault(locale);
+                Configuration config = new Configuration();
+                config.locale = locale;
+                getBaseContext().getResources().updateConfiguration(config, getBaseContext().getResources().getDisplayMetrics());
+
+
+                Intent intent = new Intent(MainActivity.this, MainActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+
+            }
+        });
+
+    }
+}
