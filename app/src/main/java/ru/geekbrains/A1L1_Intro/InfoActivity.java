423445278c086ac8c8db44b7686dd7639d37d3d4
Sergey <Ganzalo2010@gmail.com>
2019-12-04 16:39:42 +0300

    add some features

---
+++ app/src/main/java/ru/geekbrains/A1L1_Intro/InfoActivity.java
@@ -0,0 +1,14 @@
+package ru.geekbrains.A1L1_Intro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info);
+    }
+}
